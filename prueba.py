10f40c2a94906b1a8581724721d5c2e4f2019ef7
Juan C. Hernandez <juan.hdzcrr@gmail.com>
2021-12-09 19:54:05 -0600

    Se crea prueba de funcionamiento de Voz

--- prueba.py
+++ prueba.py
@@ -1,53 +1,13 @@
 import numpy as np
 import reconocimiento_patrones as rp
-import voz
-import os
-
-
-def nombres_archivos(path, ext):
-    archivos = os.listdir(path)
-    return [x for x in archivos if x.endswith(ext)]
-
-
-def obtiene_nombres_archivos():
-    archivos_finales = set()
-    while True:
-        sel = input("Buscar archivos en directorio actual? [S/n]: ")
-        ruta = '.'
-        if sel == 'n' or sel == 'N':
-            ruta = input("Escribe la ruta: ")
-        
-        ext = input("extensión de archivo: ")
-        
-        nombres = nombres_archivos(ruta, ext)
-        print(f"Se encontraron {len(nombres)} archivos con extensión {ext}:")
-        for nombre in nombres:
-            print(nombre)
-            
-        sel = input("Añadir archivos? [S/n]: ")
-        if sel == 'n' or sel == 'N':
-            continue
-    
-        archivos_finales.update(nombres)
-    
-        sel = input("Agregar mas archivos? [s/N]: ")
-        if not (sel == 'S' or sel == 's'):
-            return archivos_finales
-
-
-def procesa_audio(nombre_archivos, M, N, grado):
-    audios = []
-    for nombre in nombre_archivos:
-        audios.append(voz.Voz(nombre, M, N, grado))
-        
-    return audios
+import procesamiento_voz as pv
 
 
 if __name__ == '__main__':
-    nombres = obtiene_nombres_archivos()
+    nombres = pv.obtiene_nombres_archivos()
     print(f"Se encontraron {len(nombres)} archivos:")
     for nombre in nombres:
         print(nombre)
-    audios = procesa_audio(nombres, 10, 12, 8)
+    audios = pv.procesa_audio(nombres, 10, 12, 8)
     for audio in audios:
         print(str(audio))
